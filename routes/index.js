ed3a72ff1b0b277c660804b78add82254ec7a201
Jakob Clements <jakobclements95@gmail.com>
2020-07-06 15:05:38 -0400

    Add content to home page, start work on contact form

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
+// File: routes/index.js
+// Author: Jakob Clements
+// Project: COMP2068 Assignment 1 - Express Portfolio Site
+// Description: Contains routes to each page of the site
+
 'use strict';
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About me' });
+});
+
+/* GET projects page. */
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function (req, res) {
+    res.render('services', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
